e8eba580e6f36d892ab73b78bb13a1d2bfca4285
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-09 22:54:34 +0300

    add 1 task almost

--- Homework1/1/Program.cs
+++ Homework1/1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int number1 = Prompt("Input first number");
+int number2 = Prompt("Input second number");
+int min;
+int max;
+
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+
+}
+else
+{
+    max = number2;
+    min = number1;
+}
+
+
+Console.WriteLine 
