f474bb70fff3583642e0f6e0c8c00c129851c47e
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-10 11:45:21 +0300

    2task has completed

--- Homework1/2/Program.cs
+++ Homework1/2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int number1 = Prompt("Input first number ");
+int number2 = Prompt("Input second number ");
+int number3 = Prompt("Input second number ");
+
+int Maxvalue = number1;
+
+if (number2 > Maxvalue)
+{
+    Maxvalue = number2;
+}
+
+if (number3 > Maxvalue)
+{
+    Maxvalue = number3;
+}
+Console.Write($"Max number is {Maxvalue}");
