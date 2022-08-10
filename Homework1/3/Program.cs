9111e88a8ab75a87312eea209196ceaf8e2e5cb4
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-10 12:26:17 +0300

    3task has completed

--- Homework1/3/Program.cs
+++ Homework1/3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int number = Prompt("Input number ");
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
+else
+{
+    Console.WriteLine("The number is odd");
+}
