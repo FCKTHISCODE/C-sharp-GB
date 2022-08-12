d21514694f04e69c0f5c06be68b532361a75bb50
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-12 11:35:35 +0300

    1task has been completed

--- Homework2/1/Program.cs
+++ Homework2/1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int number = Prompt("Input three digits number number ");
+if (number >= 100 && number < 1000)
+{
+    int print_result = number / 10 % 10;
+    System.Console.Write (print_result);
+}
+else
+{
+    System.Console.Write("It is not 3 digits number");
+}
