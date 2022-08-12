0012ebc6a928d2fc9308b85e2ce8a56fcdd6601c
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-12 16:08:14 +0300

    3task has been completed

--- Homework2/3/Program.cs
+++ Homework2/3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int Number = Prompt("Введи число  ");
+
+if (Number > 99)
+{
+    while (Number > 1000)
+    {
+       Number = Number / 10;
+    }
+    int result = Number % 10;
+    System.Console.Write(result);
+}
+else
+{
+    System.Console.Write("нет Третьей цифры");
+}
