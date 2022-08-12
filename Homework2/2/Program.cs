b6144350b6e8ddecaac9320294b4aee11f50f991
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-12 11:50:18 +0300

    2task has been completed

--- Homework2/2/Program.cs
+++ Homework2/2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100,1000);
+
+System.Console.WriteLine (number);
+
+int First_digit = number/100%10;
+int Second_digit = number/10%10;
+int Third_digit = number%10;
+
+int result =  First_digit*10 + Third_digit;
+System.Console.WriteLine (result);
