ff7e0bb2723ed7f48df9c99a8a6b12d7e872ce30
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-12 12:55:13 +0300

    4task has been completed

--- Homework2/4/Program.cs
+++ Homework2/4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int Week_Day;
+Week_Day = Prompt("Введи номер дня недели ");
+
+if (Week_Day < 1 || Week_Day > 7)
+{
+    System.Console.WriteLine("Это не день недели ! ");
+    return;
+}
+
+if (Week_Day == 6 || Week_Day == 7)
+{
+    System.Console.WriteLine("Да, выходной");
+}
+else
+{
+    System.Console.WriteLine("Нет, рабочий");
+}
