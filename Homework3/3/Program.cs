4370595897581c58feff99e250e1ca9bab08302d
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-16 11:01:18 +0300

    3 completed

---
+++ Homework3/3/Program.cs
@@ -0,0 +1,18 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+void Numcubed (int number)
+{
+    for (int i=1; i <= number; i++)
+    {
+    Console.WriteLine (i*i*i);
+    }
+}
+
+int enterednumber = Prompt ("Enter a number");
+Numcubed (enterednumber);
