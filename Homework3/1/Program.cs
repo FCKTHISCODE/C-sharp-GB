23d980c8843f4a8fbe4c92501e4867504cdd3043
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-17 12:30:40 +0300

    1 completed

---
+++ Homework3/1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Enter 5 digit number ");
+string inputnumber = System.Console.ReadLine();
+
+if (inputnumber [0] == inputnumber [4] && inputnumber [1] == inputnumber [3])
+{
+    System.Console.WriteLine("Palindromic number");
+}
+
+else
+{
+    System.Console.WriteLine("Not Palindromic number");
+}
