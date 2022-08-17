891f1d22bf9f2b80ea3aa128095b15199c42f76f
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-17 23:52:07 +0300

    Update Program.cs

--- Homework3/4/Program.cs
+++ Homework3/4/Program.cs
@@ -1,16 +1,1 @@
-﻿void Reversestring(string reverstr)
-{
-    for (int i,tmp = 0, j = str.Length (reverstr)-1;i++,j--)
-    {
-        tmp = string [i];
-        string [i] = string [j];
-        string [j] = tmp;
-    }
-}
-//string reverce_enterednumber2=
 
-
-string enterednumber1 = Prompt ("Enter a number");
-///string enterednumber2 = Prompt("Enter a number");
-string reverce_enterednumber1 = Reversestring (enterednumber1);
-System.Console.WriteLine (reverce_enterednumber1);
