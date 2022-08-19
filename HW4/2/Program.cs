49eb52c60f6d9bb1aafa24950210708b58dd9a46
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-19 22:57:33 +0300

    2 done

---
+++ HW4/2/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+int summacifr(int num)
+{
+    int summa = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        num = num / 10;
+        summa += digit;
+    }
+    return summa;
+}
+
+int number = Prompt("Введи число ");
+Console.WriteLine(summacifr(number));
