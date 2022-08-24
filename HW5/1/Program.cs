494b1837f1cdf85856229a230a8d93e3db6315fe
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-24 23:12:53 +0300

    first done

---
+++ HW5/1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int length)
+{
+    int[] answer = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(100, 999);
+    }
+    return answer;
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+void PrintArray(int[] array) // вывод массива на консоль
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+
+}
+
+int EvenNumberCount(int[] array)
+    {
+        int evenCount = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        return evenCount;
+    }
+
+
+int ArrayLenghtInput = Prompt ("Введи длину массива  ");
+int [] randomArray  = CreateArray (ArrayLenghtInput);
+PrintArray (randomArray);
+Console.Write ("количество чётных чисел в массиве  ");
+System.Console.WriteLine (EvenNumberCount(randomArray));
+
+
+
+
