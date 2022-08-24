0f6680d9db24d701003f8429b7e86e4a40e7b159
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-24 23:45:06 +0300

    second done

---
+++ HW5/2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] answer = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(min, max + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array) // вывод массива на консоль
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int SumOddNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+
+
+int ArrayLenght = Prompt("ВВеди длину массива  ");
+int min_Array = Prompt("ВВеди минимум в массиве  ");
+int max_Array = Prompt("ВВеди максимум в массиве  ");
+
+int[] ARRAY = CreateArray(ArrayLenght, min_Array, max_Array);
+PrintArray(ARRAY);
+
+Console.WriteLine(SumOddNumbers(ARRAY));
