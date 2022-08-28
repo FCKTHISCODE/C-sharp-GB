15d1868b5b47d0df47a4a3e11c43fb66d64fd3a9
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-08-28 14:15:13 +0300

    HW 6 done

--- HW6/1/Program.cs
+++ HW6/1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter multiple numbers using ,  ");
+
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(','), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Количество положительных чисел: " + count);
--- HW6/2/Program.cs
+++ HW6/2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+
+////////////////////////////////////////////////////
+int k1 = Prompt("Enter k1:  ");
+int k2 = Prompt("Enter k2:  ");
+int b1 = Prompt("Enter b2:  ");
+int b2 = Prompt("Enter b2:  ");
+
+if (k1 == k2)
+{
+    if (k1 == k2 && b1 == b2) 
+    Console.WriteLine($"Прямые совпадают");
+    
+    else Console.WriteLine($"Прямые параллельны");
+}
+else
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"Точка пересечения: ({x}; {y})");
+}
