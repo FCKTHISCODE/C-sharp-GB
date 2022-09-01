f96a810c27ac842fbb4fb826c1470d87dbba0bff
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-09-01 14:36:12 +0300

    2 task has been completed

---
+++ HW7/2/Program.cs
@@ -0,0 +1,59 @@
+﻿int[,] CreateMatrix(int rows, int columns, int minNumber, int maxNumber)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(minNumber, maxNumber + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++) //m
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++) //n
+        {
+            if (j == 0) Console.Write(" | ");
+            Console.Write(mtrx[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+
+int ArrayRows = Prompt("Введи число строк  ");
+int ArrayColumns = Prompt("Введи чичло колонок ");
+int min_Array = Prompt("Введи минимум в массиве ");
+int max_Array = Prompt("Введи максимум в массиве ");
+
+int findingNumberRow = Prompt("Введи cтроку числа, какого ищешь ");
+int findingNumberColumn = Prompt("Введи колонку числа, какого ищешь ");
+
+int[,] GenereatedMatrix = CreateMatrix(ArrayRows, ArrayColumns, min_Array, max_Array);
+PrintMatrix(GenereatedMatrix);
+
+
+
+if (findingNumberRow > 0 && findingNumberRow <= ArrayRows && findingNumberColumn > 0
+&& findingNumberColumn <= ArrayColumns)
+{
+    Console.WriteLine($"{GenereatedMatrix[findingNumberRow-1, findingNumberColumn-1]}");
+}
+else
+{
+    Console.WriteLine("нет такого элемеента в массиве");
+}
+
