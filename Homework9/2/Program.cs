12a2e276b6ff9c9e6175285344c167850db70964
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-09-05 23:10:38 +0300

    2 task has been completed

---
+++ Homework9/2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int SummFromMtoN(int m, int n)
+{
+    if (n > m)
+        return n + SummFromMtoN(m, n - 1);
+    return m;
+}
+
+int m = Prompt("Введи число M  ");
+int n = Prompt("Введи число N  ");
+
+int Sum = SummFromMtoN(m, n);
+Console.WriteLine($"Сумма чисел от M до N: " + Sum);
