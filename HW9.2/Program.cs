ba80126eea0b6360e76d73b2bd7b5546842e9192
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-09-05 23:03:37 +0300

    2 task has been completed

---
+++ HW9.2/Program.cs
@@ -0,0 +1,23 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int Ackermann (int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Ackermann(n - 1, 1);
+    else
+        return Ackermann(n - 1, Ackermann(n, m - 1));
+}
+
+int m = Prompt("Введи число M  ");
+int n = Prompt("Введи число N  ");
+
+Console.WriteLine(Ackermann(m,n));
